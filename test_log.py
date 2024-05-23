f2fb8a4e1136545194279e08eb1701ca86a47463
inesvar <90340779+inesvar@users.noreply.github.com>
2024-05-23 22:42:57 +0200

    play (todo: allow underline and bold)

--- test_log.py
+++ test_log.py
@@ -7,7 +7,7 @@ def test():
     log.setLogLevel(log.LogLevels.TRACE)
     # the color has to exist in https://dslackw.gitlab.io/colored/tables/colors
     log.setSecondaryColor('gray')
-    log.setSecondaryColor('dark_gray')
+    log.setSecondaryColor('red')
     # the header string has to be formattable
     log.setHeader("{filename}:{lineno}{reset} -> {color}{log_na")
     log.setHeader("{filename}:{lineno}{reset} -> {color}{log_na}")
@@ -16,27 +16,32 @@ def test():
     # there has to be 5 valid colors from the table mentioned above
     log.setLogColors(['red', 'orange', 'green', 'white'])
     log.setLogColors(['red', 'orange', 'green', 'white', 'gray'])
+    log.reset()
+    print()
 
 
 def main():
-    log.setLogLevel(log.LogLevels.TRACE)
     # supports multiple arguments separated by commas like python's print
-    log.error("lol", 5 + 4, "hey")
+    log.error("Division of", 54 % 27, "by", 2 - 2, "is not allowed")
 
     # you can optionally specify `sep` and `end`
-    log.warn("lol", 5, "hey", sep=", ")
-    log.info("lol", 5, "hey", end="\n\n")
+    log.warn(*["fruit", "vegetable", "fish", "meat"],
+             sep=", ", end=" weren't found in the fridge\n")
+    log.info("OPERATION SUCCESS", end="\n\n")
+
+    # you can filter the logs you want to see
+    log.debug("This will not be printed")
+    # default log level is `INFO`
+    log.setLogLevel(log.LogLevels.TRACE)
+    i = 254
+    log.debug("i =", i)
 
     # you can output to a file (without the colors)
-    file = open("aha", "w")
-    log.debug("lol", 5, "hey", file=file)
-    log.debug("lol", 5, "hey")
+    file = open("log", "w")
+    log.debug("This will be printed to a file", file=file)
 
-    # and of course you can specify the logs you want to see
-    log.trace("lol", 5, "hey")
-    log.setLogLevel(log.LogLevels.INFO)
-    log.trace("should not be printed")
+    log.trace("Entire content of a list :", list(range(15)))
 
 
-# test()
+test()
 main()
